4bb54a9fb071b7ab8f6ef2070d9d0f03ec0f2a50
Gianfoke <56170561+Gianfoke@users.noreply.github.com>
2021-11-26 12:58:08 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,21 +1,7 @@
-ol.proj.proj4.register(proj4);
-ol.proj.get("EPSG:32632").setExtent([604814.509305, 4972194.075417, 624200.349045, 4983140.390098]);
 var wms_layers = [];
 
 
-        var lyr_BingMap_0 = new ol.layer.Tile({
-            'title': 'Bing Map',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'http://ecn.dynamic.t0.tiles.virtualearth.net/comp/CompositionHandler/{q}?mkt=en-us&it=G,VE,BX,L,LA&shading=hill'
-            })
-        });
-
-        var lyr_GoogleHybrid_1 = new ol.layer.Tile({
+        var lyr_GoogleHybrid_0 = new ol.layer.Tile({
             'title': 'Google Hybrid',
             'type': 'base',
             'opacity': 1.000000,
@@ -26,38 +12,29 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
             })
         });
-
-        var lyr_GoogleHybrid_2 = new ol.layer.Tile({
-            'title': 'Google Hybrid',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' &middot; <a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
-                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
-            })
-        });
-var format_tronchi_po_3 = new ol.format.GeoJSON();
-var features_tronchi_po_3 = format_tronchi_po_3.readFeatures(json_tronchi_po_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:32632'});
-var jsonSource_tronchi_po_3 = new ol.source.Vector({
+var format_fondale_1 = new ol.format.GeoJSON();
+var features_fondale_1 = format_fondale_1.readFeatures(json_fondale_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_fondale_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_tronchi_po_3.addFeatures(features_tronchi_po_3);
-var lyr_tronchi_po_3 = new ol.layer.Vector({
+jsonSource_fondale_1.addFeatures(features_fondale_1);
+var lyr_fondale_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_tronchi_po_3, 
-                style: style_tronchi_po_3,
+                source:jsonSource_fondale_1, 
+                style: style_fondale_1,
                 interactive: true,
-                title: '<img src="styles/legend/tronchi_po_3.png" /> tronchi_po'
-            });
+    title: 'fondale<br />\
+    <img src="styles/legend/fondale_1_0.png" /> 200<br />\
+    <img src="styles/legend/fondale_1_1.png" /> 240<br />\
+    <img src="styles/legend/fondale_1_2.png" /> <br />'
+        });
 
-lyr_BingMap_0.setVisible(true);lyr_GoogleHybrid_1.setVisible(true);lyr_GoogleHybrid_2.setVisible(true);lyr_tronchi_po_3.setVisible(true);
-var layersList = [lyr_BingMap_0,lyr_GoogleHybrid_1,lyr_GoogleHybrid_2,lyr_tronchi_po_3];
-lyr_tronchi_po_3.set('fieldAliases', {'id': 'id', 'Nome': 'Nome', 'gg_240': 'gg_240', 'gg_220': 'gg_220', });
-lyr_tronchi_po_3.set('fieldImages', {'id': 'TextEdit', 'Nome': 'TextEdit', 'gg_240': 'TextEdit', 'gg_220': 'TextEdit', });
-lyr_tronchi_po_3.set('fieldLabels', {'id': 'header label', 'Nome': 'header label', 'gg_240': 'header label', 'gg_220': 'header label', });
-lyr_tronchi_po_3.on('precompose', function(evt) {
+lyr_GoogleHybrid_0.setVisible(true);lyr_fondale_1.setVisible(true);
+var layersList = [lyr_GoogleHybrid_0,lyr_fondale_1];
+lyr_fondale_1.set('fieldAliases', {'id': 'id', 'luogo': 'luogo', 'valore': 'valore', });
+lyr_fondale_1.set('fieldImages', {'id': 'TextEdit', 'luogo': 'TextEdit', 'valore': 'TextEdit', });
+lyr_fondale_1.set('fieldLabels', {'id': 'header label', 'luogo': 'header label', 'valore': 'header label', });
+lyr_fondale_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1,35 +1,4 @@
 
-isTracking = false;
-var geolocateControl = (function (Control) {
-    geolocateControl = function(opt_options) {
-        var options = opt_options || {};
-        var button = document.createElement('button');
-        button.className += ' fa fa-map-marker';
-        var handleGeolocate = function() {
-            if (isTracking) {
-                map.removeLayer(geolocateOverlay);
-                isTracking = false;
-          } else if (geolocation.getTracking()) {
-                map.addLayer(geolocateOverlay);
-                map.getView().setCenter(geolocation.getPosition());
-                isTracking = true;
-          }
-        };
-        button.addEventListener('click', handleGeolocate, false);
-        button.addEventListener('touchstart', handleGeolocate, false);
-        var element = document.createElement('div');
-        element.className = 'geolocate ol-unselectable ol-control';
-        element.appendChild(button);
-        ol.control.Control.call(this, {
-            element: element,
-            target: options.target
-        });
-    };
-    if (Control) geolocateControl.__proto__ = Control;
-    geolocateControl.prototype = Object.create(Control && Control.prototype);
-    geolocateControl.prototype.constructor = geolocateControl;
-    return geolocateControl;
-}(ol.control.Control));
 
 var measuring = false;
 var measureControl = (function (Control) {
@@ -93,27 +62,32 @@ var expandedAttribution = new ol.control.Attribution({
 
 var map = new ol.Map({
     controls: ol.control.defaults({attribution:false}).extend([
-        expandedAttribution,new measureControl(),new geolocateControl()
+        expandedAttribution,new measureControl()
     ]),
     target: document.getElementById('map'),
     renderer: 'canvas',
     overlays: [overlayPopup],
     layers: layersList,
     view: new ol.View({
-         maxZoom: 28, minZoom: 1, projection: new ol.proj.Projection({
-            code: 'EPSG:32632',
-            extent: [-20037508.342789, -20037508.342789, 20037508.342789, 20037508.342789],
-            units: 'm'})
+         maxZoom: 28, minZoom: 1
     })
 });
 
-var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
-map.addControl(layerSwitcher);
-layerSwitcher.hidePanel = function() {};
-layerSwitcher.showPanel();
 
+    var searchLayer = new SearchLayer({
+      layer: lyr_fondale_1,
+      colName: 'luogo',
+      zoom: 10,
+      collapsed: true,
+      map: map
+    });
 
-map.getView().fit([604814.509305, 4972194.075417, 624200.349045, 4983140.390098], map.getSize());
+    map.addControl(searchLayer);
+    document.getElementsByClassName('search-layer')[0]
+    .getElementsByTagName('button')[0].className +=
+    ' fa fa-binoculars';
+    
+map.getView().fit([1170965.939154, 5600565.571183, 1189495.744485, 5613130.472782], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
@@ -153,8 +127,8 @@ var featureOverlay = new ol.layer.Vector({
     updateWhileInteracting: true // optional, for instant visual feedback
 });
 
-var doHighlight = true;
-var doHover = true;
+var doHighlight = false;
+var doHover = false;
 
 var highlight;
 var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
@@ -639,55 +613,6 @@ var formatLength = function(line) {
 addInteraction();
 
 
-      var geolocation = new ol.Geolocation({
-  projection: map.getView().getProjection()
-});
-
-
-var accuracyFeature = new ol.Feature();
-geolocation.on('change:accuracyGeometry', function() {
-  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-});
-
-var positionFeature = new ol.Feature();
-positionFeature.setStyle(new ol.style.Style({
-  image: new ol.style.Circle({
-    radius: 6,
-    fill: new ol.style.Fill({
-      color: '#3399CC'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#fff',
-      width: 2
-    })
-  })
-}));
-
-geolocation.on('change:position', function() {
-  var coordinates = geolocation.getPosition();
-  positionFeature.setGeometry(coordinates ?
-      new ol.geom.Point(coordinates) : null);
-});
-
-var geolocateOverlay = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [accuracyFeature, positionFeature]
-  })
-});
-
-geolocation.setTracking(true);
-
-
-var geocoder = new Geocoder('nominatim', {
-  provider: 'osm',
-  lang: 'en-US',
-  placeholder: 'Search for ...',
-  limit: 5,
-  keepOpen: true
-});
-map.addControl(geocoder);
-
-document.getElementsByClassName('gcd-gl-btn')[0].className += ' fa fa-search';
 
 var attributionComplete = false;
 map.on("rendercomplete", function(evt) {
